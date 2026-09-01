604ed9eaa229ad9edce491afc14c9a7529742691
Ankur33657 <ankursingh943040@gmail.com>
2026-09-02 00:27:54 +0530

    feat/add-GRPC client and server

--- patient-service/src/main/java/com/patientmanagementsystem/patientservice/service/patient/PatientServiceImpl.java
+++ patient-service/src/main/java/com/patientmanagementsystem/patientservice/service/patient/PatientServiceImpl.java
@@ -3,6 +3,7 @@ package com.patientmanagementsystem.patientservice.service.patient;
 
 import com.patientmanagementsystem.patientservice.Dto.patient.PatientRequestDto;
 import com.patientmanagementsystem.patientservice.Dto.patient.PatientResponseDto;
+import com.patientmanagementsystem.patientservice.grpc.BillingServiceGrpcClient;
 import com.patientmanagementsystem.patientservice.modules.Patient;
 import com.patientmanagementsystem.patientservice.repository.PatientRepository;
 import lombok.AllArgsConstructor;
@@ -22,6 +23,7 @@ import java.util.UUID;
 public class PatientServiceImpl implements PatientService {
 
     private  PatientRepository patientRepository;
+    private BillingServiceGrpcClient billingServiceGrpcClient;
 
     @Override
     public List<PatientResponseDto> getAllPatients() {
@@ -37,6 +39,8 @@ public class PatientServiceImpl implements PatientService {
     @Override
     public PatientResponseDto createNewPatient(PatientRequestDto patient) {
         Patient p=patientRepository.save(PatientRequestDto.toPatientModel(patient));
+        billingServiceGrpcClient.createBillingAccount(p.getId().toString(),p.getName(),p.getEmail());
+
         return PatientResponseDto.toPatientResponseDto(p);
     }
 
